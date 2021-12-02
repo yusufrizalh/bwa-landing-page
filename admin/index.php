c35a4dd7e518dc1f7f1db7e7112380c6ae3412f1
Yusuf Rizal <yusufrizalh@gmail.com>
2021-12-02 14:31:50 +0700

    dashboard admin profile

--- admin/index.php
+++ admin/index.php
@@ -8,22 +8,21 @@
     <title>Login Page</title>
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="./admin.css">
 </head>
 
-<body>
+<body class="text-center">
     <div class="container">
-        <br>
-        <h3>Login Administrator</h3>
-        <form action="./loginaction.php" method="post">
-            <div class="form-group">
-                <label for="email">Email address</label>
-                <input type="email" class="form-control" name="email" placeholder="Enter your email address">
-            </div>
-            <div class="form-group">
-                <label for="password">Password</label>
-                <input type="password" class="form-control" name="password" placeholder="Enter your password">
-            </div>
-            <button type="submit" class="btn btn-primary" name="btnlogin">Login</button>
+        <form class="form-signin" method="POST" action="./loginaction.php">
+            <img class="mb-4" src="https://i.ibb.co/PxkfQPX/github-avatar.png" alt="" width="210" height="210">
+            <h1 class="h3 mb-3 font-weight-normal">Login Administrator</h1>
+            <label for="inputEmail" class="sr-only">Email</label>
+            <input type="email" name="email" class="form-control" placeholder="Enter email address" required autofocus>
+            <label for="inputPassword" class="sr-only">Password</label>
+            <input type="password" name="password" class="form-control" placeholder="Enter your password" required>
+
+            <button class="btn btn-lg btn-info btn-block" type="submit" name="btnlogin">Login</button>
         </form>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
